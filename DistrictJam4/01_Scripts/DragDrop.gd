5519a42c04c71281037dcf2264709d9086d70e1e
LucasFranciscoC <chacon100flamengo@hotmail.com>
2023-06-24 14:01:38 -0300

    grid feito

--- DistrictJam4/01_Scripts/DragDrop.gd
+++ DistrictJam4/01_Scripts/DragDrop.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 var selected = false
 var distance = 40
+var slotLayer0 = 318
+var slotLayer1 = 278
+var slotLayer2 = 238
+var slotLayer3 = 198
+var slotLayer4 = 158
 
 func _ready():
 	pass
@@ -13,10 +18,60 @@ func _process(delta):
 
 func followMouse():
 	get_parent().position = get_global_mouse_position()
-	if get_parent().position.distance_to(Vector2(38,282)) < distance:
-		get_parent().position = Vector2(38,282)
-	if get_parent().position.distance_to(Vector2(85,282)) < distance:
-		get_parent().position = Vector2(85,282)
+	if get_parent().position.distance_to(Vector2(48,slotLayer0)) < distance:
+		get_parent().position = Vector2(48,slotLayer0)
+	if get_parent().position.distance_to(Vector2(88,slotLayer0)) < distance:
+		get_parent().position = Vector2(88,slotLayer0)
+	if get_parent().position.distance_to(Vector2(128,slotLayer0)) < distance:
+		get_parent().position = Vector2(128,slotLayer0)
+	if get_parent().position.distance_to(Vector2(168,slotLayer0)) < distance:
+		get_parent().position = Vector2(168,slotLayer0)
+	if get_parent().position.distance_to(Vector2(208,slotLayer0)) < distance:
+		get_parent().position = Vector2(208,slotLayer0)
+	
+	if get_parent().position.distance_to(Vector2(48,slotLayer1)) < distance:
+		get_parent().position = Vector2(48,slotLayer1)
+	if get_parent().position.distance_to(Vector2(88,slotLayer1)) < distance:
+		get_parent().position = Vector2(88,slotLayer1)
+	if get_parent().position.distance_to(Vector2(128,slotLayer1)) < distance:
+		get_parent().position = Vector2(128,slotLayer1)
+	if get_parent().position.distance_to(Vector2(168,slotLayer1)) < distance:
+		get_parent().position = Vector2(168,slotLayer1)
+	if get_parent().position.distance_to(Vector2(208,slotLayer1)) < distance:
+		get_parent().position = Vector2(208,slotLayer1)
+	
+	if get_parent().position.distance_to(Vector2(48,slotLayer2)) < distance:
+		get_parent().position = Vector2(48,slotLayer2)
+	if get_parent().position.distance_to(Vector2(88,slotLayer2)) < distance:
+		get_parent().position = Vector2(88,slotLayer2)
+	if get_parent().position.distance_to(Vector2(128,slotLayer2)) < distance:
+		get_parent().position = Vector2(128,slotLayer2)
+	if get_parent().position.distance_to(Vector2(168,slotLayer2)) < distance:
+		get_parent().position = Vector2(168,slotLayer2)
+	if get_parent().position.distance_to(Vector2(208,slotLayer2)) < distance:
+		get_parent().position = Vector2(208,slotLayer2)
+	
+	if get_parent().position.distance_to(Vector2(48,slotLayer3)) < distance:
+		get_parent().position = Vector2(48,slotLayer3)
+	if get_parent().position.distance_to(Vector2(88,slotLayer3)) < distance:
+		get_parent().position = Vector2(88,slotLayer3)
+	if get_parent().position.distance_to(Vector2(128,slotLayer3)) < distance:
+		get_parent().position = Vector2(128,slotLayer3)
+	if get_parent().position.distance_to(Vector2(168,slotLayer3)) < distance:
+		get_parent().position = Vector2(168,slotLayer3)
+	if get_parent().position.distance_to(Vector2(208,slotLayer3)) < distance:
+		get_parent().position = Vector2(208,slotLayer3)
+	
+	if get_parent().position.distance_to(Vector2(48,slotLayer4)) < distance:
+		get_parent().position = Vector2(48,slotLayer4)
+	if get_parent().position.distance_to(Vector2(88,slotLayer4)) < distance:
+		get_parent().position = Vector2(88,slotLayer4)
+	if get_parent().position.distance_to(Vector2(128,slotLayer4)) < distance:
+		get_parent().position = Vector2(128,slotLayer4)
+	if get_parent().position.distance_to(Vector2(168,slotLayer4)) < distance:
+		get_parent().position = Vector2(168,slotLayer4)
+	if get_parent().position.distance_to(Vector2(208,slotLayer4)) < distance:
+		get_parent().position = Vector2(208,slotLayer4)
 	pass
 
 

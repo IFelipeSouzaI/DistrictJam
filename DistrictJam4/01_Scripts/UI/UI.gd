3582d5eb410b9ac407f7f0d3751c99365c17737c
LucasFranciscoC <chacon100flamengo@hotmail.com>
2023-06-24 23:20:24 -0300

    ui update

---
+++ DistrictJam4/01_Scripts/UI/UI.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _ready():
+	pass
+
+
+func _on_StartBattle_pressed():
+	pass # Replace with function body.
+
+
+func _on_Guide_pressed():
+	pass # Replace with function body.
